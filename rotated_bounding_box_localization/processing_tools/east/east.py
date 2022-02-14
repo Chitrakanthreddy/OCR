f262b89bfa286a4bf79af88845cea6f4ad4da3e9
Chitrakanth Reddy <chitrakanthreddy@gmail.com>
2022-02-14 22:35:40 +0530

    complete pipeline

--- rotated_bounding_box_localization/processing_tools/east/east.py
+++ rotated_bounding_box_localization/processing_tools/east/east.py
@@ -36,44 +36,44 @@ def decode_predictions(scores, geometry, minConf=0.5):
             if score < minConf:
                 continue
 
-        # compute the offset factor as our resulting feature
-        # maps will be 4x smaller than the input image
-        (offsetX, offsetY) = (x * 4.0, y * 4.0)
+            # compute the offset factor as our resulting feature
+            # maps will be 4x smaller than the input image
+            (offsetX, offsetY) = (x * 4.0, y * 4.0)
 
-        # extract the rotation angle for the prediction and
-        # then compute the sin and cosine
-        angle = anglesData[x]
-        cos = np.cos(angle)
-        sin = np.sin(angle)
+            # extract the rotation angle for the prediction and
+            # then compute the sin and cosine
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
 
-        # use the geometry volume to derive the width and height
-        # of the bounding box
-        h = xData0[x] + xData2[x]
-        w = xData1[x] + xData3[x]
+            # use the geometry volume to derive the width and height
+            # of the bounding box
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
 
-        # use the offset and angle of rotation information to
-        # start the calculation of the rotated bounding box
-        offset = ([
-            offsetX + (cos * xData1[x]) + (sin * xData2[x]),
-            offsetY - (sin * xData1[x]) + (cos * xData2[x])])
-        # derive the top-right corner and bottom-right corner of
-        # the rotated bounding box
-        topLeft = ((-sin * h) + offset[0], (-cos * h) + offset[1])
-        topRight = ((-cos * w) + offset[0], (sin * w) + offset[1])
+            # use the offset and angle of rotation information to
+            # start the calculation of the rotated bounding box
+            offset = ([
+                offsetX + (cos * xData1[x]) + (sin * xData2[x]),
+                offsetY - (sin * xData1[x]) + (cos * xData2[x])])
+            # derive the top-right corner and bottom-right corner of
+            # the rotated bounding box
+            topLeft = ((-sin * h) + offset[0], (-cos * h) + offset[1])
+            topRight = ((-cos * w) + offset[0], (sin * w) + offset[1])
 
-        # compute the center (x, y)-coordinates of the rotated
-        # bounding box
-        cX = 0.5 * (topLeft[0] + topRight[0])
-        cY = 0.5 * (topLeft[1] + topRight[1])
+            # compute the center (x, y)-coordinates of the rotated
+            # bounding box
+            cX = 0.5 * (topLeft[0] + topRight[0])
+            cY = 0.5 * (topLeft[1] + topRight[1])
 
-        # our rotated bounding box information consists of the
-        # center (x, y)-coordinates of the box, the width and
-        # height of the box, as well as the rotation angle
-        box = ((cX, cY), (w, h), -1 * angle * 180.0 / np.pi)
+            # our rotated bounding box information consists of the
+            # center (x, y)-coordinates of the box, the width and
+            # height of the box, as well as the rotation angle
+            box = ((cX, cY), (w, h), -1 * angle * 180.0 / np.pi)
 
-        # update our detections and confidences lists
-        rects.append(box)
-        confidences.append(score)
+            # update our detections and confidences lists
+            rects.append(box)
+            confidences.append(score)
 
     # return a 2-tuple of the bounding boxes and associated
     # confidences
